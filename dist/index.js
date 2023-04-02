b8e8894cba1b493e174f041fe0e119b644205d97
Yoo-uijin <timetam24@naver.com>
2023-04-02 17:18:36 +0900

    studying..

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let sales = 123456789;
+let course = "TypeScipt";
+let user = [1, "Jin"];
+console.log(user[1].toUpperCase());
+let mySize = 1;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let sales = 123_456_789;
+let course = "TypeScipt";
+
+let user: [number, string] = [1, "Jin"];
+console.log(user[1].toUpperCase());
+
+// PascalCase
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Small;
+console.log(mySize);
